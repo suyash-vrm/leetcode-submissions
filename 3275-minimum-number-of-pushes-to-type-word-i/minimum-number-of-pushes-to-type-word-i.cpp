d5931ae8e635ae7e4e786ba2151e2186726a4f10
Suyash Verma <suyash_v@ch.iitr.ac.in>
2026-07-30 06:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (77.73%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int n = word.size();
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            ans += i / 8 + 1;
-        }
-        return ans;
+        int m = (n - 1) / 8 + 1;
+        return m * (m - 1) * 4 + (n - (m - 1) * 8) * m;
     }
 };
